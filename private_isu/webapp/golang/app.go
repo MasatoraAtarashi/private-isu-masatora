7c57f86c6fe5697bd01500871cf4800e8e876532
MasatoraAtarashi <atarashi.masatora@gmail.com>
2022-11-27 02:28:45 +0900

    Fix

--- private_isu/webapp/golang/app.go
+++ private_isu/webapp/golang/app.go
@@ -386,7 +386,7 @@ func getIndex(w http.ResponseWriter, r *http.Request) {
 
 	results := []Post{}
 
-	err := db.Select(&results, "SELECT `id`, `user_id`, `body`, `mime`, `created_at` FROM `posts` ORDER BY `created_at` DESC LIMIT $1", postsPerPage)
+	err := db.Select(&results, "SELECT `id`, `user_id`, `body`, `mime`, `created_at` FROM `posts` ORDER BY `created_at` DESC LIMIT ?", postsPerPage)
 	if err != nil {
 		log.Print(err)
 		return
